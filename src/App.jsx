b7136c37ee3a070561feefada88e3895390828da
SvitlanaLucky <svitlanalucky04@gmail.com>
2025-05-06 16:42:43 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-import Heading from './components/Heading/Heading';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import { lazy } from 'react';
+
+const Home = lazy(() => import('./pages/Home'));
+const Rates = lazy(() => import('./pages/Rates'));
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="/rates" element={<Rates />} />
+        <Route />
+      </Route>
+    </Routes>
+  );
 };
